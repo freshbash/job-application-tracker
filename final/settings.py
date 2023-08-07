f9bee05c0e00710d26faed4003ebac71f5b30c38
Bhashwar <bhaswarsengupta@gmail.com>
2023-08-07 16:48:23 +0530

    added additional files and folders

--- final/settings.py
+++ final/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'application_tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
