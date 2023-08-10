cc6e28a47ae5157d6e06fe8713b10809905b015f
Bhashwar <bhaswarsengupta@gmail.com>
2023-08-10 14:48:25 +0530

    user authentication implemented at the backend

--- application_tracker/views/user_auth.py
+++ application_tracker/views/user_auth.py
@@ -1,2 +1,65 @@
+from application_tracker.models import User
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+
+
+#View to render the register page
+def registration_page(request):
+    if request.method == "GET":
+        return render(request, "application_tracker/registration_page.html")
+
+#View to render login page
+def login_page(request):
+    if request.method == "GET":
+        return render (request, "application_tracker/login_page.html")
+
+#View to handle user registration
+def register(request):
+    if request.method == "POST":
+        #Get the new user's data
+        username = request.POST["username"]
+        password = request.POST["password"]
+        website = request.POST["website"]
+        email = request.POST["email"]
+        linkedin = request.POST["linkedin"]
+
+        #Create a new user
+        new_user = User.objects.create_user(username=username, password=password, website_url=website, email_url=email, linkedin_url=linkedin)
+
+        #Save the new user
+        new_user.save()
+
+        #Log the new user in
+        login(request, new_user)
+
+        #Redirect the user to the home page
+        return HttpResponseRedirect(reverse("index"))
+    
+#View to handle user login
+def login(request):
+    if request.method == "POST":
+        
+        #Get login data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        #Authenticate the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            #Log the user in
+            login(request, user)
+
+            #Redirect the user to the home page
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            #Redirect the user to the login page
+            return render(request, "application_tracker/login_page.html", {"message": "Incorrect username/password"})
+        
+#View to handle user logout
+def logout(request):
+    logout(request)
+
+    #Redirect the user to the landing page
+    return HttpResponseRedirect(reverse("index"))
